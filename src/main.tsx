472359c3513089cb172121e51debc0be261a3158
karthikeyant <tkarthikeyan@gmail.com>
2026-09-23 21:49:20 +0530

    feat: integrate Vercel Speed Insights into application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import { initConsoleSignature } from "./utils/consoleSignature";
 
 initConsoleSignature();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <SpeedInsights />
     <App />
   </StrictMode>
 );
